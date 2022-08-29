ef7fdf10a90ba98fb1baab6eeb03b6bb238ddc32
Nathan Mannall <44876217+NMannall@users.noreply.github.com>
2022-08-29 21:49:16 +0100

    Change to updateToken hook
    - Fog will now only reset when token elevation actually changes

--- scripts/module.js
+++ scripts/module.js
@@ -3,16 +3,30 @@
 // SPDX-License-Identifier: MIT
 import { log } from './log.js'
 
-Hooks.on('levelsOnElevationChangeUpdate', async function(_, token) {
-    let fogExploration = canvas.sight.exploration;
-
-    if (canvas.sight.pending.children.length > 1 || fogExploration.id) {
+Hooks.on("updateToken", (token, updates) => {
+    if(!canvas.tokens.get(token.id)?._controlled) return;
+    if("elevation" in updates) {
         log.info(`Token '${token.data._id}' set elevation to '${token.data.elevation}'`);
 
+        let fogExploration = canvas.sight.exploration;
         if (fogExploration.id) {
             log.info(`Deleting saved FogExploration '${fogExploration.id}'`);
             fogExploration.delete();
         }
         canvas.sight._handleResetFog();
-    } 
+    }
 });
+
+// Hooks.on('levelsOnElevationChangeUpdate', async function(_, token) {
+//     let fogExploration = canvas.sight.exploration;
+
+//     if (canvas.sight.pending.children.length > 1 || fogExploration.id) {
+//         log.info(`Token '${token.data._id}' set elevation to '${token.data.elevation}'`);
+
+//         if (fogExploration.id) {
+//             log.info(`Deleting saved FogExploration '${fogExploration.id}'`);
+//             fogExploration.delete();
+//         }
+//         canvas.sight._handleResetFog();
+//     } 
+// });
