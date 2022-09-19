192b4cf859bad8a48a4cf248db6939d801952e6b
Nathan Mannall <44876217+NMannall@users.noreply.github.com>
2022-09-19 23:31:01 +0100

    Persist FoW for different elevations

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,16 @@
+Object.defineProperty(FogExploration.prototype, "elevation", {
+    get: function () {
+        return this.flags?.fogLevels?.elevation;
+    },
+    set: function (value) {
+        this.updateSource({
+            flags: { fogLevels: { elevation: value } },
+        });
+    }
+});
+
 // Register logging levels with dev-mode module
-Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {    
+Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
     registerPackageDebugFlag("fog-levels", "level");
 });
 
@@ -11,6 +22,53 @@ Hooks.once('init', async function() {
     }
 
     Hooks.callAll("fogLevelsInit", CONFIG.FogLevels);
+
+    libWrapper.register(
+        CONFIG.FogLevels.MODULE_ID,
+        'FogExploration.get', async function (wrapped, ...args) {
+            console.log('FogLevels | canvas.fog.exploration.get was called');
+            const collection = game.collections.get("FogExploration");
+            const sceneId = (args[0]?.scene || canvas.scene)?.id || null;
+            const userId = (args[0]?.user || game.user)?.id;
+            const elevation = CONFIG.Levels?.currentToken?.document?.elevation;
+            if ( !sceneId || !userId || elevation === undefined) return null;
+            if ( !(game.user.isGM || (userId === game.user.id)) ) {
+                throw new Error("FogLevels | You do not have permission to access the FogExploration object of another user");
+            }
+            
+            console.log("FogLevels | args: ", args);
+            console.log("FogLevels | sceneId: ", sceneId);
+            console.log("FogLevels | userId: ", userId);
+            console.log("FogLevels | elevation: ", elevation);
+
+            // Return cached exploration
+            let exploration = collection.find(x => (x.user.id === userId) && (x.scene.id === sceneId) && (x.elevation == elevation));
+            if ( exploration ) {
+                console.log("FogLevels | Found cached exploration");
+                return exploration;
+            }
+
+            // Return persisted exploration
+            let options = args[1] || {};
+            const response = await this.database.get(this, {
+                query: {scene: sceneId, user: userId},
+                options: options
+            });
+            const explorations = response.filter(x => {
+                return x.elevation === elevation;
+            })
+            exploration = explorations.length ? explorations.shift() : null;
+            if ( exploration ) {
+                console.log("FogLevels | Found persisted exploration");
+                collection.set(exploration.id, exploration);
+            } else {
+                console.log("FogLevels | Found no exploration");
+            }
+            return exploration;
+        },        
+        'OVERRIDE'
+    );
+
     Hooks.callAll("fogLevelsReady", CONFIG.FogLevels);
 });
 
--- scripts/module.js
+++ scripts/module.js
@@ -1,10 +1,19 @@
 import { log } from './log.js'
 
-// Main control hook. Resets FoW when token elevation changes.
-// TODO: Save FogExploration per level.
-Hooks.on("updateToken", (token, updates) => {
+// Main control hook. Loads FoW for current token elevation.
+Hooks.on("updateToken", async function (token, updates) {
     if(game.user.isGM) return;
     if(token.isOwner && "elevation" in updates) {
-        canvas.fog._handleReset();
+        log.info(`Token '${token.actorId}' set elevation to '${token.elevation}'`);
+        await canvas.fog.commit();
+        await canvas.fog.save();
+        await canvas.fog.load();
+        canvas.fog.exploration.elevation = token.elevation;
+        return;
+    }
+    if (canvas.fog.exploration.elevation == null) {  // Attempt to load FoW if elevation for current FoW is null and set elevation.
+        await canvas.fog.load();
+        canvas.fog.exploration.elevation = token.elevation;
+        return;
     }
 });
